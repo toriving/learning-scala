283dca9f793cfa687bbff7d9e915f9fe7c1d5700
toriving <toriving@gmail.com>
2020-05-27 02:21:32 +0900

    Updated ch5 exercise

--- src/main/scala/com/learningScala/chapter/five/exercise_2/second.scala
+++ src/main/scala/com/learningScala/chapter/five/exercise_2/second.scala
@@ -1,5 +1,32 @@
 package com.learningScala.chapter.five.exercise_2
+import util.Random.nextInt
 
-object second {
+object second extends App{
+  /**
+   * 라이브러리 함수 util.Random.nextInt는 무작위로 정수를 반환한다.
+   * 주어진 두 개의 정수 중 큰 수를 반환하는 'max' 함수를 호출할 때 이 함수를 사용하여
+   * 임의의 두개의 정수 중 큰 수를 반환하도록 하라.
+   * 두개의 정수 중 작은 수를 반환하는 함수로 동일한 작업을 해보고,
+   * 매번 두 번째 정수를 반환하는 함수로 동일한 작업을 수행하라.
+   */
+
+  def max(x: Int, y: Int):Int = if (x > y) x else y
+  def min(x: Int, y: Int):Int = if (x > y) y else x
+  def second(x: Int, y: Int):Int = y
+
+  val x1 = nextInt()
+  val y1 = nextInt()
+  println(x1, y1)
+  println(max(x1, y1))
+
+  val x2 = nextInt()
+  val y2 = nextInt()
+  println(x2, y2)
+  println(min(x2, y2))
+
+  val x3 = nextInt()
+  val y3 = nextInt()
+  println(x3, y3)
+  println(max(x3, y3))
 
 }
--- src/main/scala/com/learningScala/chapter/five/exercise_4/fourth.scala
+++ src/main/scala/com/learningScala/chapter/five/exercise_4/fourth.scala
@@ -1,5 +1,16 @@
 package com.learningScala.chapter.five.exercise_4
 
-object fourth {
+object fourth extends App{
+  /**
+   * 여러분이 다른 개발자의 코드를 검토하다가 이 함수를 보게 되었다고 가정하자.
+   * def fzero[A](x: A)(f: A => Unit): A = { f(x); x }
+   * 이 함수는 무엇을 수행하는가? 어떻게 이 함수를 호출할 것인지 예를 들 수 있는가?
+   */
+  def fzero[A](x: A)(f: A => Unit): A = { f(x); x }
 
+  println(fzero[Boolean](false) { a => println(s"a was $a") })
+  println(fzero[Int](52) { b => println(s"b was $b") })
+  println(fzero[String]("Dongju") { c => println(s"c was $c") })
+
+  // A타입의 x와 A타입을 arg로 갖는 함수x를 arg로 받아서 x에 대해 함수 f를 시행하고 리턴으로 x를 돌려줌
 }
