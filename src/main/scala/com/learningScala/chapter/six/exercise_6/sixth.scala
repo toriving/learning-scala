ac6d3b894018283f0350d6a65e7a3277a3d32eb9
toriving <toriving@gmail.com>
2020-06-04 00:04:05 +0900

    Update ch10

--- src/main/scala/com/learningScala/chapter/six/exercise_6/sixth.scala
+++ src/main/scala/com/learningScala/chapter/six/exercise_6/sixth.scala
@@ -5,13 +5,13 @@ object sixth {
   def palindrome(l:List[String]): (List[String], List[String]) ={
     l.partition(x => x == x.reverse)
   }
-//(List[String], List[String])
+
   def palindrome2(l:List[String]): (List[String], List[String]) ={
     l.foldLeft((List[String](), List[String]())){ (list, word) => if (word == word.reverse) (word :: list._1, list._2) else (list._1, word :: list._2)}
   }
 
   def main(args: Array[String]) ={
-    val pallies = List("Hi", "otto", "yo", "racecar")
+    val pallies = List("Hi", "rreerr", "danny", "ammomma")
     println(palindrome(pallies))
     println(palindrome2(pallies))
   }
