8a78f86b0ae86c1f1a179a09014409b1f381ec93
toriving <toriving@gmail.com>
2020-06-11 01:47:31 +0900

    Update ch7

--- src/main/scala/com/learningScala/chapter/seven/exercise_6/sixth.scala
+++ src/main/scala/com/learningScala/chapter/seven/exercise_6/sixth.scala
@@ -26,4 +26,56 @@ package com.learningScala.chapter.seven.exercise_6
 
 object sixth {
 
+  def githubRss(user: String, repo: String, branch: String): String = {
+    val url = s"https://github.com/$user/$repo/commits/$branch.atom"
+    val lines = io.Source.fromURL(url).getLines.toList
+    val xml = lines.map(_.trim).mkString("")
+    xml
+  }
+
+  def xmlToEntryList(xml: String): Array[String] = xml.split("</?entry>").filterNot(_.isEmpty).tail
+
+
+  def child(xml: String, name: String): Option[String] = {
+    val p = s".*<$name>(.*)</$name>.*".r
+    xml match {
+      case p(result) => Option(result)
+      case _ => None
+    }
+  }
+
+  def report(entryXml: String): Option[String] = {
+    for {
+      title <- child(entryXml, "title")
+      date <- child(entryXml, "updated").map(_.replaceAll("T.*",""))
+      author <- child(entryXml, "name")
+    }
+      yield s"title:  $title\ndate:   $date\nauthor: $author"
+  }
+
+  def getGithubReport(user: String, repo: String, branch: String): String = {
+    val xml = githubRss(user, repo, branch)
+    val entries = xmlToEntryList(xml).toList
+    val formattedEntries = entries flatMap report
+    val title = s"Github commit activity for $repo:$branch"
+    title :: formattedEntries mkString ("\n" + "=" * 80 + "\n")
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+    val xml = githubRss("toriving", "learning-scala", "master")
+
+    val entries = xmlToEntryList(xml); println(s"Got ${entries.size} entries")
+    val firstTitle = child(entries(0), "title")
+    val firstReport = report(entries(0))
+    val slickReport = getGithubReport("toriving", "learning-scala", "master")
+
+    println(xml)
+    println(entries)
+    println(firstTitle)
+    println(firstReport)
+    println(slickReport)
+
+  }
 }
