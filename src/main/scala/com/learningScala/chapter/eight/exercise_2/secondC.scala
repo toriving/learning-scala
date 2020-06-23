b3619d1784048d8d779127cdf044cd9a3e633501
toriving <toriving@gmail.com>
2020-06-23 23:04:52 +0900

    Update exercise eight

--- src/main/scala/com/learningScala/chapter/eight/exercise_2/secondC.scala
+++ src/main/scala/com/learningScala/chapter/eight/exercise_2/secondC.scala
@@ -17,15 +17,8 @@ object secondC {
   abstract class LinkedList[T] {
     def foreach(f: T => Unit): Unit
     def apply(index: Int): Option[T]
-  }
-
-  class DefinedList[T](items: T*) extends LinkedList[T] {
-    val headOption: Option[T] = items.headOption
-    lazy val head: T = items.headOption.get
-
-    val tail: Option[LinkedList[T]] = {
-      if (items.tail.isEmpty) Some(new EmptyList[T]) else Some(new DefinedList[T](items.tail: _*))
-    }
+    lazy val head: T = apply(0).get
+    lazy val tail: T = apply(size-1).get
 
     lazy val size: Int = {
       var count = 0
@@ -33,15 +26,47 @@ object secondC {
       count
     }
 
+    def filter(f: T => Boolean): LinkedList[T] = {
+      var result: LinkedList[T] = new EmptyList[T]
+      foreach { i =>
+        if ( f(i) ) {
+          result = new DefinedList[T](i)
+        }
+      }
+      result.reverse
+    }
+
+    def map[A](f: T => A): LinkedList[A] = {
+      var result: LinkedList[A] = new EmptyList[A]
+      foreach { i =>
+        result = new DefinedList[A](f(i))
+      }
+      result.reverse
+    }
+
+    lazy val reverse: LinkedList[T] = {
+      var result: LinkedList[T] = new EmptyList[T]
+      foreach { i => result = new DefinedList[T](i) }
+      result
+    }
+  }
+
+  class DefinedList[T](items: T*) extends LinkedList[T] {
+    val headOption: Option[T] = items.headOption
+
+    val next: Option[LinkedList[T]] = {
+      if (items.tail.isEmpty) Some(new EmptyList[T]) else Some(new DefinedList[T](items.tail: _*))
+    }
+
     override def foreach(f: T => Unit): Unit = {
       if (headOption.isDefined) {
         f(headOption.get)
-        tail.get.foreach(f)
+        next.get.foreach(f)
       }
     }
 
     def apply(index: Int): Option[T] = {
-      if (index == 0) headOption else tail.flatMap(_.apply(index - 1))
+      if (index == 0) headOption else next.flatMap(_.apply(index - 1))
     }
 
   }
@@ -62,6 +87,9 @@ object secondC {
     println(ll(4))
     println(ll(5))
     println(ll.head)
+    println(ll.tail)
     println(ll.size)
+    val a = ll.filter(x => x >1)
+    println("1")
   }
 }
